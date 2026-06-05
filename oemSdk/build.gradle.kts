811fc4e9cb1af4365f42bb6fd5ee1e5ce47c6ae1
Vikas <vikas.kumar1@yuma.energy>
2026-06-05 17:50:58 +0530

    Bump version to 1.0.8-beta

--- oemSdk/build.gradle.kts
+++ oemSdk/build.gradle.kts
@@ -151,7 +151,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.vikas-kmr1"
                 artifactId = "oem_sdk_beta"
-                version = "1.0.7-beta"
+                version = "1.0.8-beta"
             }
         }
     }
