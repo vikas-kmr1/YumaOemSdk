ccb7487d080183b80578e7703cbf9293e48e0893
Vikas <vikas.kumar1@yuma.energy>
2026-06-09 14:48:43 +0530

    updated: oem_sdk_beta version to 1.0.14-beta added: POST_NOTIFICATIONS permission to oemSdk AndroidManifest.xml removed: FOREGROUND_SERVICE_SPECIAL_USE and duplicate ACCESS_NETWORK_STATE permissions from oemSdk AndroidManifest.xml removed: POST_NOTIFICATIONS permission from app AndroidManifest.xml

--- oemSdk/build.gradle.kts
+++ oemSdk/build.gradle.kts
@@ -151,7 +151,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.vikas-kmr1"
                 artifactId = "oem_sdk_beta"
-                version = "1.0.13-beta"
+                version = "1.0.14-beta"
             }
         }
     }
